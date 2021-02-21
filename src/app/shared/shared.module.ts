d74153c3308ec2adecefe974f4c41c2690ede005
Hyman1993 <1392841128@qq.com>
2021-02-21 22:32:54 +0900

    2021.02.21 angular整合highcharts,js

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,18 +6,20 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import {FlexLayoutModule } from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatListModule} from '@angular/material/list';
 import { RouterModule } from '@angular/router';
-
+import { AreaComponent } from './widgets/area/area.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
+    AreaComponent,
   ],
   imports: [
     CommonModule,
@@ -28,12 +30,14 @@ import { RouterModule } from '@angular/router';
     FlexLayoutModule,
     MatMenuModule,
     MatListModule,
-    RouterModule
+    RouterModule,
+    HighchartsChartModule,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
+    AreaComponent,
   ]
 })
 export class SharedModule { }
