236209654ad3c50176b56ab3deb387920d6aed67
Hyman1993 <1392841128@qq.com>
2021-03-27 00:40:56 +0900

    2021.03.27 card page completed

--- src/app/shared/widgets/card/card.component.ts
+++ src/app/shared/widgets/card/card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
 
 @Component({
   selector: 'app-widget-card',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() label: string;
+  @Input() total: string;
+  @Input() percentage: string;
+  
+  Highcharts = Highcharts;
+  chartOptions = {}
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.chartOptions = {
+      chart: {
+        type: 'area',
+        backgroundColor:null,
+        borderwidth:0,
+        margin:[2,2,2,2],
+        height: 60
+    },
+    title: {
+        text: null
+    },
+    subtitle: {
+        text: null
+    },
+    tooltip: {
+        split: true,
+        outside: true
+    },
+    legend: {
+      enabled: false
+    },
+    credits: {
+     enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    yAxis: {
+       labels:{
+         enabled:false,
+       },
+       titles:{
+         text:null
+       },
+       startOntick:false,
+       endOnTick:false,
+       tickOptions:[]
+    },
+    series: [{
+          data:[71,78,39,66]
+    }]
+     };
 
+     HC_exporting(Highcharts);
+
+     setTimeout(() => {
+         window.dispatchEvent(
+           new Event('resize')
+         );
+     }, 300);
+    }
 }
